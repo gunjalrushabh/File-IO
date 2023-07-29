c7836780c35ce875225e48379af4ce2a261174e8
Rushabh Gunjal <121039241+gunjalrushabh@users.noreply.github.com>
2023-07-29 13:24:58 +0530

    Deleted last code

--- File_io/src/main/java/com/app/repository/FileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.enitity.FileEntity;
-
-public interface FileRepository extends JpaRepository<FileEntity, Long> {
-
-}
