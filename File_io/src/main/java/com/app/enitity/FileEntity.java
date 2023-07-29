d96ebd48647ed4cd910c506c71ace9e8ad4f3f46
Rushabh Gunjal <121039241+gunjalrushabh@users.noreply.github.com>
2023-07-29 13:29:47 +0530

    file io
    working code Springboot oracle

---
+++ File_io/src/main/java/com/app/enitity/FileEntity.java
@@ -0,0 +1,53 @@
+package com.app.enitity;
+
+import java.util.Arrays;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "filetbl")
+@Getter
+@Setter
+public class FileEntity {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String fileName;
+
+    @Column
+    private String fileType;
+
+    @Lob
+    @Column
+    private byte[] data;
+
+    public FileEntity() {
+    	
+    }
+
+	public FileEntity(Long id, String fileName, String fileType, byte[] data) {
+		this.id = id;
+		this.fileName = fileName;
+		this.fileType = fileType;
+		this.data = data;
+	}
+
+	@Override
+	public String toString() {
+		return "FileEntity [id=" + id + ", fileName=" + fileName + ", fileType=" + fileType + ", data="
+				+ (data != null ? data.length : 0) + "]";
+	}
+      
+}
